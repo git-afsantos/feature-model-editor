3614fa12190b2e443d86c3fe0e9f26322918ce2f
André Santos <git-afsantos@users.noreply.github.com>
2024-04-17 16:13:01 +0100

    update seed

--- src/data/seed.ts
+++ src/data/seed.ts
@@ -8,397 +8,55 @@
 const xmlInput: string = `\
 <?xml version='1.0' encoding='utf8'?>
 <featureModel>
-	<struct>
-		<and abstract="true" mandatory="true" name="/dts-v1/">
-			<feature name="memory" />
-			<alt name="cpus">
-				<feature name="cpu@0" />
-				<feature name="cpu@1" />
-			</alt>
-		</and>
-	</struct>
-	<constraints>
-		<rule>
-			<imp>
-				<var>cpus</var>
-				<var>memory</var>
-			</imp>
-		</rule>
-	</constraints>
-	<vm_config>
-		<configuration name="VM 1">
-			<feature name="/dts-v1/" manual="undefined" automatic="activated" />
-			<feature name="memory" manual="selected" automatic="undefined" />
-			<feature name="cpus" manual="selected" automatic="undefined" />			
-			<feature name="cpu@0" manual="selected" automatic="undefined" />
-			<feature name="cpu@1" manual="unselected" automatic="undefined" />
-		</configuration>
-		<configuration name="VM 2">
-			<feature name="/dts-v1/" manual="undefined" automatic="activated" />
-			<feature name="memory" manual="selected" automatic="undefined" />
-			<feature name="cpus" manual="selected" automatic="undefined" />			
-			<feature name="cpu@0" manual="unselected" automatic="undefined" />
-			<feature name="cpu@1" manual="selected" automatic="undefined" />
-		</configuration>
-	</vm_config>
+  <struct>
+    <and abstract="true" mandatory="true" name="Breakfast">
+      <feature name="Coffee" />
+      <alt name="Eggs">
+        <feature name="Hard-boiled" />
+        <feature name="Scrambled" />
+        <feature name="Omelette" />
+      </alt>
+      <and name="Oatmeal">
+        <feature name="Fruits" />
+        <feature name="Nuts" />
+        <feature name="Topping" />
+      </and>
+    </and>
+  </struct>
+  <constraints>
+    <rule>
+      <imp>
+        <var>Topping</var>
+        <var>Coffee</var>
+      </imp>
+    </rule>
+  </constraints>
+  <vm_config>
+    <configuration name="High Protein">
+      <feature name="Breakfast" manual="undefined" automatic="activated" />
+      <feature name="Coffee" manual="unselected" automatic="undefined" />
+      <feature name="Eggs" manual="selected" automatic="undefined" />      
+      <feature name="Hard-boiled" manual="unselected" automatic="undefined" />
+      <feature name="Scrambled" manual="unselected" automatic="undefined" />
+      <feature name="Omelette" manual="selected" automatic="undefined" />
+      <feature name="Oatmeal" manual="selected" automatic="undefined" />      
+      <feature name="Fruits" manual="selected" automatic="undefined" />
+      <feature name="Nuts" manual="selected" automatic="undefined" />
+      <feature name="Topping" manual="unselected" automatic="undefined" />
+    </configuration>
+    <configuration name="High Energy">
+      <feature name="Breakfast" manual="undefined" automatic="activated" />
+      <feature name="Coffee" manual="selected" automatic="undefined" />
+      <feature name="Eggs" manual="selected" automatic="undefined" />      
+      <feature name="Hard-boiled" manual="unselected" automatic="undefined" />
+      <feature name="Scrambled" manual="selected" automatic="undefined" />
+      <feature name="Omelette" manual="unselected" automatic="undefined" />
+      <feature name="Oatmeal" manual="selected" automatic="undefined" />      
+      <feature name="Fruits" manual="selected" automatic="undefined" />
+      <feature name="Nuts" manual="selected" automatic="undefined" />
+      <feature name="Topping" manual="selected" automatic="undefined" />
+    </configuration>
+  </vm_config>
 </featureModel>`;
 
 export default xmlInput;
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Data
-////////////////////////////////////////////////////////////////////////////////
-
-export const fmConfigInput: string = `\
-{
-	"cross_tree_constraints": [],
-  "feature_tree_constraints": [],
-  "verification_constraint": { "tag": "Var", "contents": "root" },
-  "program": {
-		"constraints": [],
-  	"hypervisor": {
-      "tag": "Node",
-      "contents": [
-        {
-          "attrs": ["Mandatory"],
-          "featureName": "root",
-          "machine": { "tag": "Hypervisor" },
-          "state": { "tag": "Automatic", "contents": true },
-          "subtree": "Optional"
-        },
-        [
-          {
-            "tag": "Node",
-            "contents": [
-              {
-                "attrs": ["Mandatory"],
-                "featureName": "/dts-v1/",
-                "machine": { "tag": "VirtualMachine", "contents": "1" },
-                "state": { "tag": "Automatic", "contents": true },
-                "subtree": "Optional"
-              },
-              [
-                {
-                  "tag": "Node",
-                  "contents": [
-                    {
-                      "attrs": [],
-                      "featureName": "memory",
-                      "machine": { "tag": "VirtualMachine", "contents": "1" },
-                      "state": { "tag": "Manual", "contents": true },
-                      "subtree": "Optional"
-                    },
-                    []
-                  ]
-                },
-                {
-                  "tag": "Node",
-                  "contents": [
-                    {
-                      "attrs": [],
-                      "featureName": "cpus",
-                      "machine": { "tag": "VirtualMachine", "contents": "1" },
-                      "state": { "tag": "Manual", "contents": true },
-                      "subtree": "Alternative"
-                    },
-                    [
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@0",
-                            "machine": { "tag": "VirtualMachine", "contents": "1" },
-                            "state": { "tag": "Manual", "contents": true },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@1",
-                            "machine": { "tag": "VirtualMachine", "contents": "1" },
-                            "state": { "tag": "Manual", "contents": false },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@0",
-                            "machine": { "tag": "VirtualMachine", "contents": "2" },
-                            "state": { "tag": "Manual", "contents": false },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@1",
-                            "machine": { "tag": "VirtualMachine", "contents": "2" },
-                            "state": { "tag": "Manual", "contents": true },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      }
-                    ]
-                  ]
-                }
-              ]
-            ]
-          },
-          {
-            "tag": "Node",
-            "contents": [
-              {
-                "attrs": ["Mandatory"],
-                "featureName": "/dts-v1/",
-                "machine": { "tag": "VirtualMachine", "contents": "2" },
-                "state": { "tag": "Automatic", "contents": true },
-                "subtree": "Optional"
-              },
-              [
-                {
-                  "tag": "Node",
-                  "contents": [
-                    {
-                      "attrs": [],
-                      "featureName": "memory",
-                      "machine": { "tag": "VirtualMachine", "contents": "2" },
-                      "state": { "tag": "Manual", "contents": true },
-                      "subtree": "Optional"
-                    },
-                    []
-                  ]
-                },
-                {
-                  "tag": "Node",
-                  "contents": [
-                    {
-                      "attrs": [],
-                      "featureName": "cpus",
-                      "machine": { "tag": "VirtualMachine", "contents": "2" },
-                      "state": { "tag": "Manual", "contents": true },
-                      "subtree": "Alternative"
-                    },
-                    [
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@0",
-                            "machine": { "tag": "VirtualMachine", "contents": "2" },
-                            "state": { "tag": "Manual", "contents": false },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@1",
-                            "machine": { "tag": "VirtualMachine", "contents": "2" },
-                            "state": { "tag": "Manual", "contents": true },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@0",
-                            "machine": { "tag": "VirtualMachine", "contents": "1" },
-                            "state": { "tag": "Manual", "contents": true },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      },
-                      {
-                        "tag": "Node",
-                        "contents": [
-                          {
-                            "attrs": [],
-                            "featureName": "cpu@1",
-                            "machine": { "tag": "VirtualMachine", "contents": "1" },
-                            "state": { "tag": "Manual", "contents": false },
-                            "subtree": "Optional"
-                          },
-                          []
-                        ]
-                      }
-                    ]
-                  ]
-                }
-              ]
-            ]
-          }
-        ]
-      ]
-    },
-  	"instances": [
-      [
-        { "tag": "VirtualMachine", "contents": "1" },
-        {
-          "tag": "Node",
-          "contents": [
-            {
-              "attrs": ["Mandatory"],
-              "featureName": "/dts-v1/",
-              "machine": { "tag": "VirtualMachine", "contents": "1" },
-              "state": { "tag": "Automatic", "contents": true },
-              "subtree": "Optional"
-            },
-            [
-              {
-                "tag": "Node",
-                "contents": [
-                  {
-                    "attrs": [],
-                    "featureName": "memory",
-                    "machine": { "tag": "VirtualMachine", "contents": "1" },
-                    "state": { "tag": "Manual", "contents": true },
-                    "subtree": "Optional"
-                  },
-                  []
-                ]
-              },
-              {
-                "tag": "Node",
-                "contents": [
-                  {
-                    "attrs": [],
-                    "featureName": "cpus",
-                    "machine": { "tag": "VirtualMachine", "contents": "1" },
-                    "state": { "tag": "Manual", "contents": true },
-                    "subtree": "Alternative"
-                  },
-                  [
-                    {
-                      "tag": "Node",
-                      "contents": [
-                        {
-                          "attrs": [],
-                          "featureName": "cpu@0",
-                          "machine": { "tag": "VirtualMachine", "contents": "1" },
-                          "state": { "tag": "Manual", "contents": true },
-                          "subtree": "Optional"
-                        },
-                        []
-                      ]
-                    },
-                    {
-                      "tag": "Node",
-                      "contents": [
-                        {
-                          "attrs": [],
-                          "featureName": "cpu@1",
-                          "machine": { "tag": "VirtualMachine", "contents": "1" },
-                          "state": { "tag": "Manual", "contents": false },
-                          "subtree": "Optional"
-                        },
-                        []
-                      ]
-                    }
-                  ]
-                ]
-              }
-            ]
-          ]
-        }
-      ],
-      [
-        { "tag": "VirtualMachine", "contents": "2" },
-        {
-          "tag": "Node",
-          "contents": [
-            {
-              "attrs": ["Mandatory"],
-              "featureName": "/dts-v1/",
-              "machine": { "tag": "VirtualMachine", "contents": "2" },
-              "state": { "tag": "Automatic", "contents": true },
-              "subtree": "Optional"
-            },
-            [
-              {
-                "tag": "Node",
-                "contents": [
-                  {
-                    "attrs": [],
-                    "featureName": "memory",
-                    "machine": { "tag": "VirtualMachine", "contents": "2" },
-                    "state": { "tag": "Manual", "contents": true },
-                    "subtree": "Optional"
-                  },
-                  []
-                ]
-              },
-              {
-                "tag": "Node",
-                "contents": [
-                  {
-                    "attrs": [],
-                    "featureName": "cpus",
-                    "machine": { "tag": "VirtualMachine", "contents": "2" },
-                    "state": { "tag": "Manual", "contents": true },
-                    "subtree": "Alternative"
-                  },
-                  [
-                    {
-                      "tag": "Node",
-                      "contents": [
-                        {
-                          "attrs": [],
-                          "featureName": "cpu@0",
-                          "machine": { "tag": "VirtualMachine", "contents": "2" },
-                          "state": { "tag": "Manual", "contents": false },
-                          "subtree": "Optional"
-                        },
-                        []
-                      ]
-                    },
-                    {
-                      "tag": "Node",
-                      "contents": [
-                        {
-                          "attrs": [],
-                          "featureName": "cpu@1",
-                          "machine": { "tag": "VirtualMachine", "contents": "2" },
-                          "state": { "tag": "Manual", "contents": true },
-                          "subtree": "Optional"
-                        },
-                        []
-                      ]
-                    }
-                  ]
-                ]
-              }
-            ]
-          ]
-        }
-      ]
-    ]
-	}
-}`;
